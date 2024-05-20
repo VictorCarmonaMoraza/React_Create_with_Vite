1ede3f35e96dded1057147c108bb03d5451f539d
Victo <victorcarmona9035@hotmail.com>
2024-05-20 22:04:28 +0100

    Tarea: Crear un nuevo componente

---
+++ counter-app/src/Tarea.jsx
@@ -0,0 +1,9 @@
+const Tarea = () => {
+    return (
+        <div>
+            <h1>Victor Manuel Carmona </h1>
+        </div>
+    );
+}
+
+export default Tarea;
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';   // Import the React library
 import ReactDOM from 'react-dom/client';
 
 import FirstAppVictor2024  from './FirstApp';
+import Tarea from './Tarea';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstAppVictor2024 />
+        {/* <FirstAppVictor2024 /> */}
+        <Tarea/>
     </React.StrictMode>
 );
