2cee1d816831e69680f8ec2a773e1b798123e123
Victo <victorcarmona9035@hotmail.com>
2024-05-20 21:35:09 +0100

    Hola Mundo en React

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';   // Import the React library
+import ReactDOM from 'react-dom/client';
+
+
+
+function App(){
+    //Document.createElement('h1');
+    return (
+        <div>
+            <h1>Hello World!</h1>;
+        </div>
+    );  
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
